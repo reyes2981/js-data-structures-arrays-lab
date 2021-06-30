9e0459bf05dd52ce2ca7a13cde092a30bede54f4
noe <noe.reyes92@yahoo.com>
2021-06-29 22:17:15 -0500

    commit tree ba1243b054b5ad92cc61fa5dd4520f60c2df0f6d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+var drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){ 
+    return drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver(name){
+    return drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(){
+    return drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+    return drivers.shift();
+}
+
+function appendDriver(name){
+    return [...drivers, name];
+}
+
+function prependDriver(name){
+    return[name, ...drivers]
+}
+
+function removeFirstDriver () {
+    return drivers.slice(1);
+  }
+
+  function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+}
